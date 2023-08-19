1a52b4f5ba5a0d21a861da49cb4cc664093fbac3
Yuri <104223310+Yuriko-Otorii@users.noreply.github.com>
2023-08-19 13:49:17 -0700

    Set up tailwindCSS

--- src/Lp.jsx
+++ src/Lp.jsx
@@ -1,6 +1,11 @@
 
 export const Lp = () => {
   return (
-    <div>LP</div>
+    <>
+      <div>LP</div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
   )
 }
