6d08dc6b962da1eb3daf646bda55013b388d72aa
Yuri <104223310+Yuriko-Otorii@users.noreply.github.com>
2023-10-02 15:59:54 -0700

    Adjust width in services section

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -6,7 +6,7 @@ export const Services = () => {
   return (
     <div id="services">
       <h2 className="text-[#145B19] text-[2rem] uppercase text-center font-bold my-[1rem] underline underline-offset-4 decoration-1 md:mt-[5rem] md:mb-[4rem]">Services</h2>
-      <div className="w-full my-[1.2rem] px-[1rem] flex flex-col items-center gap-5 md:grid md:grid-cols-2 lg:grid-cols-3">
+      <div className="w-full my-[1.2rem] px-[1rem] flex flex-col items-center gap-5 md:grid md:grid-cols-2 lg:grid-cols-3 xl:w-[80%] mx-auto">
         {services.map(service => (
           <ServiceCard key={service.id} serviceInfo={service} />
         ))}
