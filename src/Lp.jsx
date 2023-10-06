173f7691d064f66aa783eda057b6feede0beb917
Yuri <104223310+Yuriko-Otorii@users.noreply.github.com>
2023-10-06 16:55:37 -0700

    Move contact form toast to Lp component

--- src/Lp.jsx
+++ src/Lp.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import { Navbar } from "./Components/Navbar";
 import { HamburgerMenu } from "./Components/HamburgerMenu";
 import { Hero } from "./Sections/Hero";
@@ -9,7 +11,6 @@ import { AboutUs } from "./Sections/AboutUs";
 import { History } from "./Sections/History";
 import { Faq } from "./Sections/Faq";
 import { Contact } from "./Sections/Contact";
-import { useState } from "react";
 import { ContactModal } from "./Modals/ContactModal";
 import { Footer } from "./Sections/Footer";
 import { CareerModal } from "./Modals/CareerModal";
@@ -17,6 +18,16 @@ import { CareerModal } from "./Modals/CareerModal";
 export const Lp = () => {
   const [isContactModalOpen, setIsContactModalOpen] = useState(false);
   const [isCareerModalOpen, setIsCareerModalOpen] = useState(false);
+  const [isSentMessage, setIsSentMessage] = useState(false);
+
+  useEffect(() => {
+    if (isSentMessage) {
+      setIsContactModalOpen(false)
+      setTimeout(() => {
+        setIsSentMessage(false);
+      }, 3000);
+    }
+  }, [isSentMessage]);
 
   return (
     <>
@@ -38,6 +49,8 @@ export const Lp = () => {
           <ContactModal
             setIsContactModalOpen={setIsContactModalOpen}
             isContactModalOpen={isContactModalOpen}
+            isSentMessage={isSentMessage}
+            setIsSentMessage={setIsSentMessage}
           />
         )}
         {isCareerModalOpen && (
@@ -46,6 +59,33 @@ export const Lp = () => {
             isCareerModalOpen={isCareerModalOpen}
           />
         )}
+        <div
+          id="toast-simple"
+          className={`absolute left-[50%] translate-x-[-50%] z-50 flex flex-col items-center justify-center w-[90%] h-[40%] md:w-[600px] md:h-[300px] h-fit p-4 space-x-4 z-20 bg-white divide-x divide-gray-200 rounded-lg shadow space-x ease-linear duration-[300ms] ${
+            isSentMessage ? "top-[10%]" : "top-[-500px]"
+          }`}
+          role="alert"
+        >
+          <svg
+            className="w-12 h-12 md:w-20 md:h-20 md:ml-2 text-blue-600 dark:text-blue-500 rotate-45"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9"
+            />
+          </svg>
+          <div className="md:pl-4 text-2xl md:text-3xl mt-10 md'mt-0">
+            Your request sent successfully. Thank you for considering our
+            service.
+          </div>
+        </div>
       </main>
     </>
   );
--- src/Modals/ContactModal.jsx
+++ src/Modals/ContactModal.jsx
@@ -1,18 +1,7 @@
-import { useState } from "react";
 import { ContactForm } from "../Forms/ContactForm";
 import contactImg from "../assets/paint img.jpg";
-import { useEffect } from "react";
 
-export const ContactModal = ({ isContactModalOpen, setIsContactModalOpen }) => {
-  const [isSentMessage, setIsSentMessage] = useState(false);
-
-  useEffect(() => {
-    if (isSentMessage) {
-      setTimeout(() => {
-        setIsSentMessage(false);
-      }, 3000);
-    }
-  }, [isSentMessage]);
+export const ContactModal = ({ isContactModalOpen, setIsContactModalOpen, isSentMessage, setIsSentMessage }) => {
 
   return (
     <div className="flex justify-center items-center py-5 h-screen fixed inset-0 z-50 outline-none focus:outline-none">
@@ -56,32 +45,6 @@ export const ContactModal = ({ isContactModalOpen, setIsContactModalOpen }) => {
             </h2>
             <ContactForm setIsSentMessage={setIsSentMessage} />
           </div>
-        </div>
-      </div>
-      <div
-        id="toast-simple"
-        className={`absolute left-[50%] translate-x-[-50%] z-50 flex flex-col items-center justify-center w-[90%] h-[40%] md:w-[600px] md:h-[300px] h-fit p-4 space-x-4 z-20 bg-white divide-x divide-gray-200 rounded-lg shadow space-x ease-linear duration-[300ms] ${
-          isSentMessage ? "top-[10%]" : "top-[-500px]"
-        }`}
-        role="alert"
-      >
-        <svg
-          className="w-12 h-12 md:w-20 md:h-20 md:ml-2 text-blue-600 dark:text-blue-500 rotate-45"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 18 20"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9"
-          />
-        </svg>
-        <div className="md:pl-4 text-2xl md:text-3xl mt-10 md'mt-0">
-          Your request sent successfully. Thank you for considering our service.
         </div>
       </div>
     </div>
